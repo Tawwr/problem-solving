79a14019a82aa39e184279e44d463b81c999044a
Islam Sayed <islam.sayed8@gmail.com>
2022-03-28 09:38:00 +0200

    w3-d2 solution

--- index.js
+++ index.js
@@ -5,12 +5,16 @@
 // forms the first ten entries of the fibonacci series.
 
 
-
+//Iterative Solution
 function fibonacci(n) {
-
+	const fib = [1, 1];
+	for (let i = 2; i < n; i++) {
+		fib[i] = fib[i - 2] + fib[i - 1];
+	}
+	return fib[fib.length - 1];
 }
 
-fibonacci(4) //3
-fibonacci(5) //5
-fibonacci(8) //21
-fibonacci(10) //55
+fibonacci(4); //3
+fibonacci(5); //5
+fibonacci(8); //21
+fibonacci(10); //55
