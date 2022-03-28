817526526d7bd30b34083f32c612ff8bc7a3c20c
Islam Sayed <islam.sayed8@gmail.com>
2022-03-28 09:36:16 +0200

    w3-d2 problem

---
+++ index.js
@@ -0,0 +1,16 @@
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceding two.
+// For example, the sequence
+//  1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+// forms the first ten entries of the fibonacci series.
+
+
+
+function fibonacci(n) {
+
+}
+
+fibonacci(4) //3
+fibonacci(5) //5
+fibonacci(8) //21
+fibonacci(10) //55
