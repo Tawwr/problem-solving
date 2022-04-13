b6271d544475d8e7afc317f3f27679d64da21b89
Islam Sayed <islam.sayed8@gmail.com>
2022-04-13 10:35:40 +0200

    anagram solution

--- w4-d4/index.js
+++ w4-d4/index.js
@@ -1,9 +1,18 @@
-//Write a function that takes 2 string arguments and checks if 
+//Write a function that takes 2 string arguments and checks if
 //the 2 strings are anagram to each other (have the same characters with the same quant)
 //Spaces, punctuation (LIKE ? !) and capitalization should be ignored.
 
-function isAnagram(firstStr, secondStr) {
+function cleanStr(str) {
+	return str
+		.replace(/[^\w]|_/g, '')
+		.toLowerCase()
+		.split('')
+		.sort()
+		.join('');
+}
 
+function isAnagram(firstStr, secondStr) {
+	return cleanStr(firstStr) === cleanStr(secondStr);
 }
 
 isAnagram('rail safety', 'fairy tales'); // true
