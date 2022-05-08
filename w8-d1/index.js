4b6df6e63590dc8b9a15772bba47e403b565c9af
Islam Sayed <islam.sayed8@gmail.com>
2022-05-08 09:16:23 +0200

    subString problem

---
+++ w8-d1/index.js
@@ -0,0 +1,33 @@
+//Given two strings s1 and s2, return true if s1 is a substring of s2 regardless of the characters ordering,
+//or false otherwise.
+//You can expect that s1 and s2 consist of lowercase English letters.
+
+function subString(s1, s2) {
+	if (s1.length > s2.length) return false;
+	const obj = {};
+	for (let i = 0; i < s2.length; i++) {
+		if (!(s2[i] in obj)) {
+			obj[s2[i]] = 1;
+		} else {
+      obj[s2[i]]++;
+    }
+	}
+    console.log(obj)
+	for (let i = 0; i < s1.length; i++) {
+		if (!(s1[i] in obj)) {
+			return false;
+		} else {
+      obj[s1[i]]--;
+      if (obj[s1[i]] < 0) {
+        return false;
+      }
+    }
+	}
+	return true;
+}
+
+//subString('olleh', 'hello world'); // true
+//subString('olllleh', 'hello world'); // false
+//subString('a', 'b'); // false
+//subString('', 'hello'); // false
+
