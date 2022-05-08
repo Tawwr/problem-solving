9952f9625a2e328abc29326472f3b83a2725ded5
Islam Sayed <islam.sayed8@gmail.com>
2022-05-08 10:13:59 +0200

    fix: edge case

--- w8-d1/index.js
+++ w8-d1/index.js
@@ -3,27 +3,28 @@
 //You can expect that s1 and s2 consist of lowercase English letters.
 
 function subString(s1, s2) {
-	if (s1.length > s2.length) return false;
-	const obj = {};
-	for (let i = 0; i < s2.length; i++) {
-		if (!(s2[i] in obj)) {
-			obj[s2[i]] = 1;
-		} else {
-      obj[s2[i]]++;
+  s1 = s1.split(' ').join('')
+  s2 = s2.split(' ').join('')
+if (s1.length > s2.length) return false;
+const obj = {};
+for (let i = 0; i < s2.length; i++) {
+  if (!(s2[i] in obj)) {
+    obj[s2[i]] = 1;
+  } else {
+    obj[s2[i]]++;
+  }
+}
+for (let i = 0; i < s1.length; i++) {
+  if (!(s1[i] in obj)) {
+    return false;
+  } else {
+    obj[s1[i]]--;
+    if (obj[s1[i]] < 0) {
+      return false;
     }
-	}
-    console.log(obj)
-	for (let i = 0; i < s1.length; i++) {
-		if (!(s1[i] in obj)) {
-			return false;
-		} else {
-      obj[s1[i]]--;
-      if (obj[s1[i]] < 0) {
-        return false;
-      }
-    }
-	}
-	return true;
+  }
+}
+return true;
 }
 
 //subString('olleh', 'hello world'); // true
