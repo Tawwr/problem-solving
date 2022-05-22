647c556bb868f10da05ae060aa27c0eaa6282a6b
Islam Sayed <islam.sayed8@gmail.com>
2022-05-22 17:48:30 +0200

    sorted squarred array

---
+++ [21-w5-d1]sortedSquarredArray.js
@@ -0,0 +1,9 @@
+//Problem: Write a function that takes in a non-empty array of integers that are sorted
+// in ascending order and returns a new array of the same length with the squares
+// of the original integers also sorted in ascending order.
+
+//Examples: sortedSquaredArray([1, 2, 3, 5, 6, 8, 9]) //[1, 4, 9, 25, 36, 64, 81]
+
+function sortedSquaredArray(array) {
+  return array.map(num => num*num);
+}
