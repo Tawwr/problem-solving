4711814231c6c0ef5e05502762a2c9071354949a
Islam Sayed <islam.sayed8@gmail.com>
2022-05-30 10:11:26 +0200

    add 2 other solutions

--- [20-w4-d5]maxSubArray.js
+++ [20-w4-d5]maxSubArray.js
@@ -9,6 +9,25 @@
 //maxSubArray([1]) // 1
 //maxSubArray([5,4,-1,7,8]) // 23
 
+//O(n^2)
+//Nested Loops 
+function maxSubArray(nums){
+  let maxSum = nums[0];
+  for (let i = 0; i < nums.length; i++) {
+    let sum = nums[i]
+    for (let j = i + 1; j < nums.length; j++) {
+      sum += nums[j];
+      if (sum > maxSum) {
+        maxSum = sum;
+      }
+    }
+  }
+  return maxSum;
+}
+
+
+//O(n^2)
+//Sliding Window
 function maxSubArray(nums) {
   let l = 0;
   let r = nums.length - 1;
@@ -23,8 +42,27 @@ function maxSubArray(nums) {
     } else if (sum < maxSum) {
       l++;
     } else {
-      r--;
+      r--
     }
   }
   return maxSum;
 }
+
+
+//O(n)
+//Linear time complexity
+function maxSubArray(nums){
+  let maxSum = nums[0];
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (sum > maxSum) {
+      maxSum = sum;
+    }
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+  return maxSum;
+}
+
