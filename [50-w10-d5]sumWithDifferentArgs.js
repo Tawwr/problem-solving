bfdf7f02637729a82efdfb4b725e8ae947b8a064
Islam Sayed <islam.sayed8@gmail.com>
2022-05-30 23:03:23 +0200

    add sumWithDiffArgs

---
+++ [50-w10-d5]sumWithDifferentArgs.js
@@ -0,0 +1,26 @@
+//Write a sum method which will work properly when invoked using either syntax below?
+//sum(2,3) // return  5
+//sum(2)(3) // return 5
+
+
+//Solution:
+
+//#1
+function sum(x) {
+  if (arguments.length == 2) {
+    return arguments[0] + arguments[1];
+  } else {
+    return function(y) { return x + y; };
+  }
+}
+
+
+//#2
+function sum(x, y) {
+  if (y !== undefined) {
+    return x + y;
+  } else {
+    return function(y) { return x + y; };
+  }
+}
+
