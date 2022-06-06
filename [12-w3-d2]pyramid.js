404dfd5a2db17f8f3a941447ad6922190426e2af
Islam Sayed <islam.sayed8@gmail.com>
2022-06-06 11:27:05 +0200

    add solution

--- [12-w3-d2]pyramid.js
+++ [12-w3-d2]pyramid.js
@@ -1,4 +1,4 @@
-// Learning: for loop, 
+// Learning: for loop,
 // Language: javascript
 // Path: [02-w1-d2]pyramid.js
 
@@ -6,11 +6,25 @@
 //and prints a pyramid shape with n levels using the # character
 //Examples:
 /* pyramid(1); // '#'
-*  pyramid(2); // ' # '
-*                 '###'
-*  pyramid(3); // '  #  '
-*                 ' ### '
-*                 '#####'
-*/
+ *  pyramid(2); // ' # '
+ *                 '###'
+ *  pyramid(3); // '  #  '
+ *                 ' ### '
+ *                 '#####'
+ */
 
 //Solution:
+function pyramid(n) {
+	const midPoint = Math.floor((2 * n - 1) / 2);
+	for (let row = 0; row < n; row++) {
+		let level = '';
+		for (let col = 0; col < 2 * n - 1; col++) {
+			if (col + row >= midPoint && col - row <= midPoint) {
+				level += '#';
+			} else {
+				level += ' ';
+			}
+		}
+		console.log(level);
+	}
+}
