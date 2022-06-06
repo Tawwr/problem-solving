cdb5a10662c155ed67bdea0509d670e32ffa114e
Islam Sayed <islam.sayed8@gmail.com>
2022-06-06 11:28:50 +0200

    add solution

--- [42-w9-d2]moveElementToEnd.js
+++ [42-w9-d2]moveElementToEnd.js
@@ -1,26 +1,38 @@
-// Problem: You're given an array of integers and an integer. Write a function that moves
+// Problem: You're given an array of integers  and an integer. Write a function that moves
 // all instances of that integer in the array to the end of the array and returns
 // the array.
 // The function should perform this in place (i.e., it should mutate the input
 // array) and doesn't need to maintain the order of the other integers.
 
-
 //solution:
-function moveElementToEnd(array, toMove) {
-  let i = 0;
-  let j = array.length - 1;
 
-  while(i < j) {
-    while (i < j && array[j] === toMove) {
-      j--;
-    }
-    if (array[i] === toMove) {
-      const temp = array[j];
-      array[j] = array[i];
-      array[i] = temp;
-    }
-    i ++; 
-  }
-  return array;
+//#1
+function moveElementToEnd(array, toMove) {
+	let i = 0;
+	let j = array.length - 1;
+
+	while (i < j) {
+		while (i < j && array[j] === toMove) {
+			j--;
+		}
+		if (array[i] === toMove) {
+			const temp = array[j];
+			array[j] = array[i];
+			array[i] = temp;
+		}
+		i++;
+	}
+	return array;
 }
 
+
+//#2
+function moveElementToEnd(array, toMove) {
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === toMove) {
+			array.push(toMove);
+			array.splice(i, 1);
+		}
+	}
+	return array;
+}
